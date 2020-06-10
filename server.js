8220184c944a8c8220fc8d5d98b3cc5a19244168
Samrath <samrathkumarmuz@gmail.com>
2020-06-10 08:02:17 +0530

    fixed datatbase host

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./Controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-	  host : '127.0.0.1',
+	  host : 'postgresql-clear-80655',
 	  user : 'postgres',
 	  password : 12345,
 	  database : 'face-recognition'
